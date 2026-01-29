82dfa8840be8dab219bfd1064cbf3659278d623f
HarshJha1908 <hjha66252@gmail.com>
2026-01-29 15:54:44 +0530

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+import './Footer.css';
+ 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-line">
+        Copyright © 2026 · All Rights Reserved · Linde IT, Kolkata Center
+      </div>
+      <div className="footer-subline">
+        Designed and managed by Tania & Harsh
+      </div>
+    </footer>
+  );
+};
+ 
+export default Footer;
+// hlo checking
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,20 @@
 import type { ReactNode } from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/footer';
 import './MainLayout.css';
-
+ 
 type Props = {
   children: ReactNode;
 };
-
+ 
 export default function MainLayout({ children }: Props) {
   return (
-    <>
+    <div className="layout">
       <Navbar />
       <main className="main-layout">
         {children}
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
