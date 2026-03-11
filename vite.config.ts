d4ad4f9f76bee1a62c63a202bd01d1fed046cf5f
HarshJha1908 <hjha66252@gmail.com>
2026-03-11 14:42:10 +0530

    api debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+ 
 export default defineConfig({
+
   plugins: [react()],
+
+  server: {
+
+    proxy: {
+
+      '/api': {
+
+        target: 'http://10.81.70.203:81',
+
+        changeOrigin: true,
+
+        secure: false,
+
+        configure: (proxy) => {
+
+          proxy.on('proxyReq', (proxyReq) => {
+
+            proxyReq.setHeader('Connection', 'keep-alive');
+
+          });
+
+        }
+
+      }
+
+    }
+
+  }
+
 })
+
+ 
